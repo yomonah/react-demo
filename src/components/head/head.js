0165c09365d6f1f04c1ada38d031ecd03678f57e
俞梦雅 <yumengya@yaoyaodudeMacBook-Pro.local>
2017-07-23 12:24:05 +0800

    update style

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -6,19 +6,45 @@ export default class Head extends Component{
         super(props);
         this.state = {
           username:'MIA YU',
-          title:'REACT DEMO'
+          title:'REACT DEMO',
+          show:false
         }
     }
 
+    over(){
+        this.setState({show:true});
+    }
+
+    listOver(){
+        this.setState({show:true});
+    }
+
+    out(){
+        this.setState({show:false});
+    }
+
+    listOut(){
+        this.setState({show:false});
+    }
+
     render(){
-        let {username, title} = this.state;
+        let {username, title, show} = this.state;
+        let demoListCls = 'show';
+        if(!show){
+            demoListCls = 'hide';
+        }
         return(
             <div className='app-head'>
               <span className='title'>{title}</span>
-              <span className='name'>{username}</span>
-              <a className='jianshu' href='http://www.jianshu.com/u/b344d2977570' target="_blank">SIMPLE BOOK</a>
-              <a className='blog' href='https://github.com/yomonah' target="_blank">GITHUB</a>
-              <a className='home' href='https://yomonah.github.io/'>HOME</a>
+              <a className='name' href='https://github.com/yomonah' target="_blank"><i className='fa fa-github'></i>{username}</a>
+              <a className='jianshu' href='http://www.jianshu.com/u/b344d2977570' target="_blank" title='简书'>SIMPLE BOOK</a>
+              <a className='blog' href='https://github.com/yomonah' target="_blank" onMouseOver={this.over.bind(this)} onMouseOut={this.out.bind(this)}>DEMO
+              </a>
+              <a className='home' href='https://yomonah.github.io/' title='主页'>HOME</a>
+              <ul className={'demo-list ' +demoListCls} onMouseOver={this.listOver.bind(this)} onMouseOut={this.listOut.bind(this)}>
+                <a href='https://yomonah.github.io/project/app.html'>REACT DEMO</a>
+                <a>G2 DEMO</a>
+              </ul>
             </div>
         )
     }
